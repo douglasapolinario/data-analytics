95b6921bb32d6299f1104e31dbc517a7f8f1a792
Douglas Apolinario <doliveira.apolinario@gmail.com>
2018-07-10 11:47:42 -0300

    titanic

--- titanic/titanic-decision-tree.R
+++ titanic/titanic-decision-tree.R
@@ -35,3 +35,37 @@ train$Child[train$Age < 18] <- 1
 aggregate(Survived ~ Child + Sex, data=train, FUN=sum)
 aggregate(Survived ~ Child + Sex, data=train, FUN=length)
 aggregate(Survived ~ Child + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+train$Fare2 <- '30+'
+train$Fare2[train$Fare < 30 & train$Fare >= 20] <- '20-30'
+train$Fare2[train$Fare < 20 & train$Fare >= 10] <- '10-20'
+train$Fare2[train$Fare < 10] <- '<10'
+
+aggregate(Survived ~ Fare2 + Pclass + Sex, data=train, FUN=function(x) {sum(x)/length(x)})
+
+test$Survived <- 0
+test$Survived[test$Sex == 'female'] <- 1
+test$Survived[test$Sex == 'female' & test$Pclass == 3 & test$Fare >= 20] <- 0
+
+submit <- data.frame(PassengerId = test$PassengerId, Survived = test$Survived)
+write.csv(submit, file="~/Documents/MBA/data-analytics/titanic/theyallperish.csv", row.names = FALSE)
+
+library(rpart)
+fit <- rpart(Survived ~ Pclass + Sex + Age + SibSp + Parch + Fare + Embarked, 
+             data = train, method = "class")
+plot(fit)
+text(fit)
+
+install.packages('rattle')
+install.packages('rpart.plot')
+install.packages('RColorBrewer')
+library(rpart.plot)
+library(RColorBrewer)
+library(rattle)
+fancyRpartPlot(fit)
+
+Prediction <- predict(fit, test, type="class")
+submit <- data.frame(PassengerId = test$PassengerId, Survived = Prediction)
+write.csv(submit, file = "~/Documents/MBA/data-analytics/titanic/myfirstdtree.csv", row.names = FALSE)
+Prediction
+
